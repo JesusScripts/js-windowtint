f3cc7d32949a5b3200193059d1633585643f2fab
Jesus <161241841+JesusScripts@users.noreply.github.com>
2024-03-02 00:28:00 +0100

    Update s_windowtint.lua

--- s_windowtint.lua
+++ s_windowtint.lua
@@ -3,6 +3,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterUsableItem('tintchecker', function(source)
+ESX.RegisterUsableItem('tintmeter', function(source)
     TriggerClientEvent("tintmeter:useItem", source)
 end)
